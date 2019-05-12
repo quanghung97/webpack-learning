b99f4003c0d7d0c700ebabc9afe18c307ec7c416
quanghung97 <hungcan1997@gmail.com>
2019-05-12 18:26:20 +0700

    clean dist rebuild and keep unchange vendor

--- basic-05-webpack-build-reactjs-manage-products/webpack.config.js
+++ basic-05-webpack-build-reactjs-manage-products/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const VENDOR_LIBS = [
     'axios',
@@ -71,6 +72,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new CleanWebpackPlugin(),
     ]
 }
