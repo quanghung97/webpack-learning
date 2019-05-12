540f9cdbd850d664f61bf813359549e89ef236ea
quanghung97 <hungcan1997@gmail.com>
2019-05-12 17:15:58 +0700

    add bootstrap, file-loader

--- basic-05-webpack-build-reactjs-manage-products/src/index.js
+++ basic-05-webpack-build-reactjs-manage-products/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import appReducers from './reducers/index'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = createStore(
     appReducers,
--- basic-05-webpack-build-reactjs-manage-products/webpack.config.js
+++ basic-05-webpack-build-reactjs-manage-products/webpack.config.js
@@ -21,7 +21,16 @@ module.exports = {
                     'css-loader'
                 ],
                 test: /\.css$/
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|wav|mp3|ico)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {},
+                  },
+                ],
+            },
         ]
     }
 }
